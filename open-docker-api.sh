7bb9246ab4a8617dd7e42be511f689e433204db6
Anton Weiss <anton@otomato.io>
2019-12-21 17:43:46 +0000

    Add script to open docker api to the world

---
+++ open-docker-api.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+sudo mkdir -p /etc/systemd/system/docker.service.d
+# possibility 3:
+sudo tee -a /etc/systemd/system/docker.service.d/startup_options.conf  > /dev/null <<EOT
+# /etc/systemd/system/docker.service.d/override.conf
+[Service]
+ExecStart=
+ExecStart=/usr/bin/dockerd -H fd:// -H tcp://0.0.0.0:2376
+EOT
+
+sudo systemctl daemon-reload
+sudo systemctl restart docker.service
